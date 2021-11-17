15ceabfd849f86ef43852f7bc47e8f671f030cda
Usu2 Name <usu2@example.com>
2021-11-17 20:01:49 +0100

    Añado una linea en la clase principal

--- prueba1/src/prueba1/principal.java
+++ prueba1/src/prueba1/principal.java
@@ -3,7 +3,10 @@ package prueba1;
 public class principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo hola git");
+		System.out.println("Hola Mundo java-git");
+		System.out.println("Hola Mundo java-git");
+		System.out.println("Hola Mundo java-git");
+		System.out.println("Pruebas Git. Se añade Cambio1");
 
 	}
 
